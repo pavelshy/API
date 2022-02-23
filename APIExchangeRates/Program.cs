c09ad03ca0024e9d95a0b420bca989509dd7c6b7
pavelshy <pavelshymko@gmail.com>
2022-02-23 20:31:53 +0100

    first commit

---
+++ APIExchangeRates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ApiExchangeRates
+{
+
+    class Program
+    {
+        HttpClient client = new HttpClient();
+
+
+        static async Task Main(string[] args)
+        {
+            Program program = new Program();
+            await program.GetTodoItems();
+
+            Console.ReadKey();
+        }
+
+        private async Task GetTodoItems()
+        {
+            string response = await client.GetStringAsync(
+                "https://api.exchangeratesapi.io/v1/");
+
+            Console.WriteLine(response);
+
+        }
+
+    }
+
+
+
+
+
+}
