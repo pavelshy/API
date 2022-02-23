d69155a293707417b482b503e85c21c6fcb8891d
pavelshy <pavelshymko@gmail.com>
2022-02-23 21:58:15 +0100

    AddProgram

--- APIExchangeRates/Program.cs
+++ APIExchangeRates/Program.cs
@@ -10,7 +10,7 @@ namespace ApiExchangeRates
         {
             using (var client = new HttpClient())
             {
-                var endpoint = new Uri("https://jsonplaceholder.typicode.com/posts");
+                var endpoint = new Uri("http://api.exchangeratesapi.io/v1/latest?access_key=0f10eb34fd242a0c0e0025707136b73d&symbols=USD,PLN&format=1");
                 var result = client.GetAsync(endpoint).Result;
                 var json = result.Content.ReadAsStringAsync().Result;
 
